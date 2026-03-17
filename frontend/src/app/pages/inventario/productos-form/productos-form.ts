483013ff994ac277d270990e71be51940f8d821a
LeonelaRivarola <leonelarivarola6322@gmail.com>
2026-03-17 18:03:53 -0300

    issue #15: FE-201 creacion de inventario/product-form, falta

---
+++ frontend/src/app/pages/inventario/productos-form/productos-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos-form',
+  imports: [],
+  templateUrl: './productos-form.html',
+  styleUrl: './productos-form.css',
+})
+export class ProductosForm {
+  
+}
