c1c7fab0d455859e7f335a34789fbd0ae12954a1
LeonelaRivarola <leonelarivarola6322@gmail.com>
2026-04-23 09:27:47 -0300

    issue #208: FE-701 Creacion de la estructura de Presupuesto

---
+++ frontend-clean/src/app/pages/presupuestos/presupuestos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presupuestos',
+  imports: [],
+  templateUrl: './presupuestos.html',
+  styleUrl: './presupuestos.css',
+})
+export class Presupuestos {}
