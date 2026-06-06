87375cef55ca8f41b0a8deb90e4ea42caa10658c
QuispeAbel <quisperobledoabel@gmail.com>
2026-06-06 00:32:32 -0300

    test

--- frontend-clean/src/app/layout/main-layout/main-layout.spec.ts
+++ frontend-clean/src/app/layout/main-layout/main-layout.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router'; // 1. Importar provideRouter
 import { MainLayout } from './main-layout';
 
 describe('MainLayout', () => {
@@ -9,14 +9,20 @@ describe('MainLayout', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MainLayout],
+      // 2. Proveer una configuración de rutas simulada para el test
+      providers: [provideRouter([])]
     }).compileComponents();
 
     fixture = TestBed.createComponent(MainLayout);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+
+    // Opcional: Recomiendo usar detectChanges() en lugar de whenStable() 
+    // para inicializar el componente sincrónicamente en este punto.
+    fixture.detectChanges();
   });
 
   it('should create', () => {
+    // 3. Descomentar el expect, ¡ahora el test pasará!
     expect(component).toBeTruthy();
   });
 });
